d63f8814fdf27d5e98eae24a790e3c75397356c5
Sandro Munda <sandro.munda@guardis.com>
2012-08-31 09:43:01 +0200

    Added the version of nodejs in package.json

--- models/paste.js
+++ models/paste.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
-var db = mongoose.createConnection('localhost', 'yaaapaste');
+
+var db = mongoose.connect('mongodb://nodejitsu:67839f670021664a77d55d370ebe90f3@alex.mongohq.com:10075/nodejitsudb836871408122');
+//var db = mongoose.createConnection('localhost', 'yaaapaste');
 
 var pasteSchema = mongoose.Schema({
   token: {
