29ae4bfb74d36ee196c7960aa19b2698a766cf06
Sandro Munda <sandro.munda@guardis.com>
2012-09-02 21:20:39 +0200

    Replaced ejs by jade

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function compile(str, path) {
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
