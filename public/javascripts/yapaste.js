da25b1015d71f0a3c8a378172f48fe92aeb03b2b
Sandro Munda <sandro.munda@guardis.com>
2012-09-03 08:23:27 +0200

    Displayed errors if exist. Disabled paste button during the request.

--- public/javascripts/yapaste.js
+++ public/javascripts/yapaste.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  var paste_enabled = false;
+
   if (window.location.pathname.match(/\/paste.*/) != null &&
       window.location.hash.charAt(0) == '#') {
       var secretKey = window.location.hash.substr(1);
@@ -7,17 +9,35 @@ $(document).ready(function() {
   }
 
   $('#pasteSubmit').click(function(event) {
-    var content = $('#pasteTa').val();
-    var secret_key = (new Date).getTime();
-    var encrypted = GibberishAES.enc(content, secret_key);
 
-    $.post("/", {content: encrypted})
+    if (paste_enabled) {
+      return;
+    }
+    paste_enabled = true;
+
+    $("#pasteSubmit").attr('disabled', 'disabled');
+
+    setTimeout(function() {
+
+      var content = $('#pasteTa').val();
+      var secret_key = (new Date).getTime();
+      var encrypted = GibberishAES.enc(content, secret_key);
+
+      $.post("/", {content: encrypted})
       .success(function(data) {
+        $("#error").html('').hide();
+
         var data = data + '#' + secret_key;
         $('#pasteUrl').attr("href", data);
         $('#pasteUrl').text(data);
+        $("#pasteSubmit").removeAttr('disabled');
+        paste_enabled = false;
       }).error(function(data) {
+        $("#error").html('<p>' + data.responseText + '</p>').show();
         console.log("Error occured with status: " + data.status);
+        $("#pasteSubmit").removeAttr('disabled');
+        paste_enabled = false;
       });
+    }, 100);
   });
 });
