9618daa138c3f078f6c2c614b2d8a2fcfcf18d2f
Sandro Munda <sandro.munda@guardis.com>
2012-09-04 08:31:52 +0200

    Added the forkmeongithub image. Added some comments to the client-side js.

--- public/javascripts/yapaste.js
+++ public/javascripts/yapaste.js
@@ -1,56 +1,91 @@
+
+// Start the SJCL random collectors.
 sjcl.random.startCollectors();
 
+/* Generates a random string and returns it */
 function generateRandom() {
   return sjcl.codec.base64.fromBits(sjcl.random.randomWords(8, 0), 0);
 }
 
+/* Compresses the msg and returns it */
 function compress(msg) {
   return Base64.toBase64(RawDeflate.deflate(Base64.utob(msg)));
 }
 
+/* Decompresses the msg and returns it */
 function decompress(msg) {
   return Base64.btou(RawDeflate.inflate(Base64.fromBase64(msg)));
 }
 
-$(document).ready(function() {
+/* Returns true if the url location is /paste + something or not. */
+function is_paste_page() {
+  return window.location.pathname.match(/\/paste.*/) !== null;
+}
 
-  var paste_enabled = false;
-
-  if (window.location.pathname.match(/\/paste.*/) != null &&
-      window.location.hash.charAt(0) == '#') {
-      var secret_key = window.location.hash.substr(1);
-      var decrypted = decompress(sjcl.decrypt(secret_key, $('pre').text()));
-      $('pre').text(decrypted);
+/* Returns the anchor of the url (in our case, the secret key) or null. */
+function get_secret_key() {
+  if (window.location.hash.charAt(0) == '#') {
+    return window.location.hash.substr(1);
   }
 
-  $('#pasteSubmit').click(function(event) {
-    $('#pasteUrl').show();
+  return null;
+}
 
+/* Returns the full url with the token and the secret key. */
+function get_url(token, secret_key) {
+  var url = 'http://';
+  url += document.location.hostname;
+  var port = document.location.port;
+  if (port) {
+    url += ':' + port;
+  }
+  url += token + '#' + secret_key;
+
+  return url;
+}
+
+$(document).ready(function() {
+
+  // Paste page ?
+  if (is_paste_page()) {
+    // Get the secret key.
+    var secret_key = get_secret_key();
+    try {
+      // Decrypt the message and display it in the pre tag.
+      var decrypted = decompress(sjcl.decrypt(secret_key, $('pre').text()));
+      $('pre').text(decrypted);
+    } catch (err) {
+      // For now, error is just an empty paste text.
+      $('pre').text('');
+    }
+  }
+
+  // A flag to indicate if a paste task is running or not.
+  var paste_enabled = false;
+
+  $('#pasteSubmit').click(function(event) {
+    // If a paste task is already running, ignore the click.
     if (paste_enabled) {
       return;
     }
+
+    // Set the paste flag to true. A paste task is now running.
     paste_enabled = true;
 
+    // Disable the paste button.
     $("#pasteSubmit").attr('disabled', 'disabled');
 
+    // We wait 100ms in order to be sure that the animation of the disable
+    // paste submit button is done.
     setTimeout(function() {
-
       var content = $('#pasteTa').val();
       var secret_key = generateRandom();
       var encrypted = sjcl.encrypt(secret_key, compress(content));
 
-      $.post("/", {content: encrypted})
-      .success(function(data) {
+      $.post("/", {content: encrypted}).success(function(data) {
         $("#error").html('').hide();
 
-        var url = 'http://';
-        url += document.location.hostname;
-        var port = document.location.port;
-        if (port) {
-          url += ':' + port;
-        }
-        url += data + '#' + secret_key;
-
+        var url = get_url(data, secret_key);
         $('#pasteUrl').text(url);
         $('#pasteUrl').attr('href', url);
         $("#pasteSubmit").removeAttr('disabled');
