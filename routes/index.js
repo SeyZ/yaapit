6fddd54ac98f6a4097361ec9af255add73d6662a
Sandro Munda <sandro@munda.me>
2013-07-17 15:49:52 +0200

    Route index is now jshint compliant.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
+'use strict';
+var crypto = require('crypto');
 var levelup = require('level');
-var db = levelup('./yaap.db')
+var db = levelup('./yaap.db');
+
+var generateToken = function(callback) {
+  crypto.randomBytes(8, function(ex, buf) {
+    if (ex) throw ex;
+    callback(buf.toString('hex'));
+  });
+};
 
 /* Get the home page */
 exports.index = function(req, res) {
@@ -7,20 +16,17 @@ exports.index = function(req, res) {
 };
 
 /* Create a new paste. */
-exports.post_index = function(req, res) {
+exports.post_index = function (req, res) {
   var content = req.body.content;
   var burn = req.body.burn;
 
   // Limit the paste size (encrypted) to ~2MB.
-  var content_size = Buffer.byteLength(content, 'utf8');
-  if (content_size > 2000000) {
-    res.send(403, "Paste too big.");
-    return;
-  }
+  var contentSize = Buffer.byteLength(content, 'utf8');
+  if (contentSize > 2000000) { return res.send(403, 'Paste too big.'); }
 
   // Generate a random token.
   generateToken(function(token) {
-    var url_path = '/paste/' + token;
+    var urlPath = '/paste/' + token;
     var data = {
       content: req.body.content,
       burn: burn === 'true',
@@ -30,17 +36,16 @@ exports.post_index = function(req, res) {
       valueEncoding: 'json'
     }, function(err) {
       if (err) { return res.send(500); }
-      res.send(url_path);
+      res.send(urlPath);
     });
   });
 };
 
 /* Get a paste */
-exports.get_paste = function(req, res) {
+exports.get_paste = function (req, res) {
   db.get(req.params.token, {
     valueEncoding: 'json'
   }, function (err, entry) {
-    console.log(entry);
     if (entry) {
       if (entry.burn) {
         db.del(req.params.token);
@@ -49,13 +54,5 @@ exports.get_paste = function(req, res) {
     } else {
       res.send(404);
     }
-  });
-};
-
-var generateToken = function(callback) {
-  require('crypto').randomBytes(8, function(ex, buf) {
-    if (ex) throw ex;
-
-    callback(buf.toString('hex'));
   });
 };
